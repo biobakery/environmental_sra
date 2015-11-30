6c6db132311abf9b7369637e3cb9d558689b83e9
schwager-hsph <schwager@hsph.harvard.edu>
2015-11-30 11:16:35 -0500

    remove vestige

--- dcc_sra/__init__.py
+++ dcc_sra/__init__.py
@@ -1,11 +1,5 @@
 from collections import namedtuple
 
-class settings:
-    keyfile = "/home/rschwager/test_data/broad_metadata/dcc_sra/iHMP_SRA_key"
-    remote_path = "/submit/Test/"
-    remote_srv = "upload.ncbi.nlm.nih.gov"
-    user = "asp-hmp2"
-
 SubmitRecord = namedtuple("SubmitRecord", "sample prepseqs")
 PrepSeq = namedtuple("PrepSeq", "prep seq")
 
